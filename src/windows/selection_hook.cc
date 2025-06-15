36c291100bbdcab6ef071748e4bb473848ecb0a8
fullex <0xfullex@gmail.com>
2025-06-15 11:24:48 +0800

    fix: correct comments for selection states in ShouldProcessGetSelection function

--- src/windows/selection_hook.cc
+++ src/windows/selection_hook.cc
@@ -1279,9 +1279,9 @@ bool SelectionHook::ShouldProcessGetSelection()
     }
 
     // Don't get text in these states:
+    // QUNS_BUSY (2) - System is busy
     // QUNS_RUNNING_D3D_FULL_SCREEN (3) - Running in full-screen mode
-    // QUNS_BUSY (4) - System is busy
-    // QUNS_PRESENTATION_MODE (5) - Presentation mode
+    // QUNS_PRESENTATION_MODE (4) - Presentation mode
     lastResult = state != QUNS_RUNNING_D3D_FULL_SCREEN && state != QUNS_BUSY && state != QUNS_PRESENTATION_MODE;
     return lastResult;
 }
